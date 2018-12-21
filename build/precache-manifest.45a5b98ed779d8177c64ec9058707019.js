9ff9ca861047e0d9ef2b31c8553db02801bbdf5f
letnoxn <letnoxn@163.com>
2018-12-21 20:38:00 +0800

    test

---
+++ build/precache-manifest.45a5b98ed779d8177c64ec9058707019.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b12145dffcc295010cc7b93e13fc07c",
+    "url": "/static/media/job.3b12145d.png"
+  },
+  {
+    "revision": "cdaa6e10d0d5179388ae",
+    "url": "/static/css/main.4b15613c.chunk.css"
+  },
+  {
+    "revision": "1016a4230215de89f8e88afd1f5043c9",
+    "url": "/static/media/user.1016a423.png"
+  },
+  {
+    "revision": "789998c1036330b7b5c3",
+    "url": "/static/js/1.789998c1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5b01f053dc936da50c534f37060623cb",
+    "url": "/static/media/boss-active.5b01f053.png"
+  },
+  {
+    "revision": "5b01f053dc936da50c534f37060623cb",
+    "url": "/static/media/job-active.5b01f053.png"
+  },
+  {
+    "revision": "5b01f053dc936da50c534f37060623cb",
+    "url": "/static/media/msg-active.5b01f053.png"
+  },
+  {
+    "revision": "5b01f053dc936da50c534f37060623cb",
+    "url": "/static/media/user-active.5b01f053.png"
+  },
+  {
+    "revision": "cdaa6e10d0d5179388ae",
+    "url": "/static/js/main.cdaa6e10.chunk.js"
+  },
+  {
+    "revision": "0a9f968e27bda6bce96ba6ba989b5b86",
+    "url": "/static/media/boy.0a9f968e.jpg"
+  },
+  {
+    "revision": "954d645cd35859e7b7a8f45b61a94cee",
+    "url": "/static/media/bull.954d645c.jpg"
+  },
+  {
+    "revision": "8a71fa6c421647d9bde3f6772841377a",
+    "url": "/static/media/chick.8a71fa6c.jpg"
+  },
+  {
+    "revision": "1c26b97aa3c0727d00f7d312b8ed596c",
+    "url": "/static/media/fj.1c26b97a.jpg"
+  },
+  {
+    "revision": "1016a4230215de89f8e88afd1f5043c9",
+    "url": "/static/media/boss.1016a423.png"
+  },
+  {
+    "revision": "1016a4230215de89f8e88afd1f5043c9",
+    "url": "/static/media/job.1016a423.png"
+  },
+  {
+    "revision": "1016a4230215de89f8e88afd1f5043c9",
+    "url": "/static/media/msg.1016a423.png"
+  },
+  {
+    "revision": "789998c1036330b7b5c3",
+    "url": "/static/css/1.e87ede22.chunk.css"
+  },
+  {
+    "revision": "494924f576d8a5a106b4d05c0afb69c3",
+    "url": "/index.html"
+  }
+];
